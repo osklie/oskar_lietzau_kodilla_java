d58fe15a037f22e0514d07f9920407ee8657c8fb
unknown <oskarlietzau1@gmail.com>
2022-07-14 00:01:03 +0200

    first commit

--- src/test/java/com/kodilla/FirstClass.java
+++ src/test/java/com/kodilla/FirstClass.java
@@ -1,4 +1,15 @@
-package com.kodilla;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class FirstClass {
+
+public class first {
+
+    public static void main (String[] args) throws java.lang.Exception {
+        int numberOne = 30;
+        int numberTwo = 50;
+        int result = numberOne + numberTwo;
+
+        System.out.println(result);
+    }
 }
