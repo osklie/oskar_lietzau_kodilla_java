15a6240484e45aa192736743e0a4b126399c7208
unknown <oskarlietzau1@gmail.com>
2022-07-14 19:30:03 +0200

    first commit

---
+++ kodilla-testing/src/test/java/com/example/kodillatesting/KodillaTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodillatesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
