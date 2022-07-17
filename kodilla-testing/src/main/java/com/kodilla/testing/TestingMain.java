7c34d5c967d8580f498536f749e601b532d0487d
unknown <oskarlietzau1@gmail.com>
2022-07-17 08:49:02 +0200

    test calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int a = 3;
+        int b =  4;
+
+        int subtract = a - b;
+        System.out.println(subtract);
+
+        int add = a + b;
+        System.out.println(add);
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package com.kodilla.testing.calculator;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Calculator {
+
+    public static void main(String[] args) throws java.lang.Exception {
+
+        int a = 5;
+        int b = 3;
+
+        int sum = a + b;
+        int sum1 = a - b;
+
+        //add( int a, int b);
+
+        //subtract( int a, int b);
+    }
+}
+
