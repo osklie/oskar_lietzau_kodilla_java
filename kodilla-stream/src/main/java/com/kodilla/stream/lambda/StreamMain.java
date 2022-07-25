f5a7916b1352459d305561fbe87715c974152217
unknown <oskarlietzau1@gmail.com>
2022-07-25 21:00:21 +0200

    ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/lambda/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream.lambda;
 
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -20,5 +21,9 @@ public class StreamMain {                                                     //
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
+
 }
