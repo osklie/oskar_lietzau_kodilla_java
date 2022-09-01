de3758e2467357fcf1ffcc324fffa6f95bc20f5d
unknown <oskarlietzau1@gmail.com>
2022-09-01 18:36:27 +0200

    new

--- TicTacToeCD/src/main/java/TicTacToe.java
+++ TicTacToeCD/src/main/java/TicTacToe.java
@@ -1,54 +1,135 @@
 import com.example.game.gameCommand;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
 
+        gameCommand pcCommand = pcCommand();
+        String answer = "";
+
         System.out.println("Witaj w grze Tic Tac Toe :D, podaj wymiar planszy ktorą wybierasz : ");
-       // Scanner scanner = new Scanner(System.in);
-       // String line = scanner.nextLine();
-        //gameCommand command = gameCommand.of(line);
-        //gameCommand pcCommand = pcCommand();
-        char obecnySymbol = 'x';
+
         int wymiar = Integer.parseInt(new Scanner(System.in).nextLine());
         char[][] plansza = new char[wymiar][wymiar];
 
+        System.out.println("wybierz znak");
+        Scanner scanner = new Scanner(System.in);
+        char obecnySymbol = scanner.next().charAt(0);
+        System.out.println("ok wybrales : " + obecnySymbol);
 
+//        while (answer.equalsIgnoreCase("o"));
+//        if (answer.equalsIgnoreCase("x")) {
+//            System.out.println(obecnySymbol);
+//        }
 
+        boolean czyKontynuowac = true;
+        int licznikRuchow = 0 ;
+        while (czyKontynuowac && licznikRuchow<= wymiar*wymiar) {
             TicTacToe.drukujPlansze(plansza);
-            while (true) {
-//        System.out.println("zadecyduj czy chcesz byc 'X' czy moze 'O'");
-//
-//            if (command == gameCommand.x) {
-//                System.out.println("wybrales krzyzyk");
-//            }
-//            if (command == gameCommand.o) {
-//                System.out.println("Wybrales kolko");
-//            }
-//
-            System.out.println(obecnySymbol + " twoj ruch");
-            System.out.println("podaj indeks wiersza");
-            int wiersz = new Scanner(System.in).nextInt();
-            System.out.println("podaj indeks kolumny");
-            int kolumna = new Scanner(System.in).nextInt();
-            boolean ruchPrawidlowy = plansza[wiersz][kolumna] == 0;
-            if (!ruchPrawidlowy) {
-                System.out.println("Ruch nipoprawny!");
-                continue;
-            }
-            plansza[wiersz][kolumna] = obecnySymbol;
-            TicTacToe.drukujPlansze(plansza);
+            boolean ruchPoprawny = wykonajRuch(plansza, obecnySymbol);
+            if(ruchPoprawny) {
+                licznikRuchow++;
+                boolean wygranaWiersza = sprawdzWiersze(plansza, obecnySymbol);
+                boolean wygranaKolumny = sprawdzKolumny(plansza, obecnySymbol);
+                boolean wygranaSkos1 = sprawdzSkos1(plansza, obecnySymbol);
+                boolean wygranaSkos2 = sprawdzSkos2(plansza, obecnySymbol);
 
-            if (obecnySymbol == 'x') {
-                obecnySymbol = 'o';
-            } else {
-                obecnySymbol = 'x';
+
+                if (wygranaWiersza || wygranaKolumny || wygranaSkos1 || wygranaSkos2) {
+                    System.out.println("gratulacje " + obecnySymbol);
+                    czyKontynuowac = false;
+                }
+
+
+                System.out.println("you chose " + obecnySymbol);
+                System.out.println("pc chose " + pcCommand);
+//                    if (obecnySymbol == 'x') {
+//                        obecnySymbol = 'o';
+//                    } else {
+//                        obecnySymbol = 'x';
+//                    }
+//            } catch (Exception e) {
+//                System.out.println(e.getMessage());
+//            }
             }
         }
 
+    }
+
+    public static boolean sprawdzSkos1(char[][] plansza, char symbol) {
+        int wymiar = plansza.length;
+        for (int i = 0; i<wymiar; i++){
+            if (plansza[i][i] != symbol) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean sprawdzSkos2(char[][] plansza, char symbol) {
+        int wymiar = plansza.length;
+        for (int i = 0; i < wymiar; i++) {
+            if (plansza[i][wymiar - i - 1] != symbol) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
+    private static boolean sprawdzWiersze(char[][] plansza, char symbol) {
+        int wymiar = plansza.length;
+        for (int wiersz = 0; wiersz < wymiar; wiersz++) {
+            boolean wygrana = true;
+            for (int kolumna = 0; kolumna < wymiar; kolumna++) {
+                if (plansza[wiersz][kolumna] != symbol) {
+                    wygrana = false;
+                    break;
+                }
+            }
+            if (wygrana) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean sprawdzKolumny(char[][] plansza, char symbol) {
+        int wymiar = plansza.length;
+        for (int kolumna = 0; kolumna < wymiar; kolumna++) {
+            boolean wygrana = true;
+            for (int wiersz = 0; wiersz < wymiar; wiersz++) {
+                if (plansza[wiersz][kolumna] != symbol) {
+                    wygrana = false;
+                    break;
+                }
+            }
+            if (wygrana) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+    public static boolean wykonajRuch(char[][] plansza, char symbol) {
+        System.out.println(symbol + " twoj ruch");
+        System.out.println("podaj indeks wiersza");
+        int wiersz = new Scanner(System.in).nextInt();
+        System.out.println("podaj indeks kolumny");
+        int kolumna = new Scanner(System.in).nextInt();
+        boolean ruchPrawidlowy = plansza[wiersz][kolumna] == 0;
+        if (!ruchPrawidlowy) {
+            System.out.println("Ruch nipoprawny!");
+            return false;
+        }
+        plansza[wiersz][kolumna] = symbol;
+        return true;
     }
 
 
@@ -74,8 +155,16 @@ public class TicTacToe {
     }
 
     private static gameCommand pcCommand() {
-    return null;
+        List<gameCommand> allowedCommands = Arrays.asList(gameCommand.one, gameCommand.two, gameCommand.three);
+
+
+        return allowedCommands.get(getRandomNumber(0, 3));
     }
 
 
+    static int getRandomNumber( int min, int max) {
+        return (int) ((Math.random() * (max - min) + min));
+    }
 }
+
+
--- game/src/main/java/com/example/game/gameCommand.java
+++ game/src/main/java/com/example/game/gameCommand.java
@@ -2,11 +2,13 @@ package com.example.game;
 
 public enum gameCommand {
     ROCK("1"), PAPER("2"), SCISSORS("3"), LIZARD("4"), SPOCK("5"),
-    quitTheGame("6"), NEWGAME("7"), YES("YES"), NO("NO"), wybierzZnak("8"), x("x"), o("o");
+    quitTheGame("6"), NEWGAME("7"), YES("YES"), NO("NO"), wybierzZnak("8"), x("x"), o("o"),
+    one("1"), two("2"), three("3");
 
 
 
     private final String value;
+
 
     gameCommand(String value) {
         this.value = value;
