1630a3442d76deb789537155e5bdf5555006a759
unknown <oskarlietzau1@gmail.com>
2022-09-11 17:51:07 +0200

    new

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoardTestSuite {
+    @Test
+    public void testTaskAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+
+        //When
+        String taskToDo = "task to do";
+        String taskInProgress = "task in progress";
+        String taskDone = "task done";
+        board.getToDoList().addTask(taskToDo);
+        board.getInProgressList().addTask(taskInProgress);
+        board.getDoneList().addTask(taskDone);
+
+        //Then
+        Assert.assertEquals("task to do", board.getToDoList().getTasks().get(0));
+        Assert.assertEquals("task in progress", board.getInProgressList().getTasks().get(0));
+        Assert.assertEquals("task done", board.getDoneList().getTasks().get(0));
+    }
+}
