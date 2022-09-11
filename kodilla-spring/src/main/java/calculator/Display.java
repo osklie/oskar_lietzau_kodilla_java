196bb732c444219ed42a76bb73c28b674ed9cbf7
unknown <oskarlietzau1@gmail.com>
2022-09-11 17:10:17 +0200

    new

---
+++ kodilla-spring/src/main/java/calculator/Display.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Display {
+
+    public void displayValue(double val) {
+        System.out.println("Value: " + val);
+    }
+}
--- kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
@@ -2,13 +2,14 @@ package com.kodilla;
 
 import com.kodilla.spring.shape.Circle;
 import com.kodilla.spring.shape.Shape;
+import com.kodilla.spring.shape.Square;
 import com.kodilla.spring.shape.Triangle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class KodillaSpringApplicationTests {
@@ -43,6 +44,18 @@ class KodillaSpringApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testSquareLoadedIntoContainer() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = context.getBean(Square.class);
+        //When
+        String name = shape.getShapeName();
+        //Then
+        assertEquals("This is a square.", name);
     }
 
 }
