9248d2cc03e6eb18de03c3cdbe893a03aa67218a
unknown <oskarlietzau1@gmail.com>
2022-09-26 21:08:31 +0200

    new

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.hibernate;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KodillaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
