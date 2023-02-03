707510c29a2ac9dabb113f185b4d3b9145cf3111
oskar <oskarlietzau1@gmail.com>
2023-02-03 20:42:23 +0100

    Merge remote-tracking branch 'origin/new' into new

---
+++ testing2/src/main/java/facebook/FacebookTestingApp.java
@@ -0,0 +1,35 @@
+package facebook;
+
+import config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+    public static final String XPATH_WAITFOR = "//select[1]";
+    public static final String XPATH_SELECT = "//div[contains(@class, \"_5k_5\")]/span/span";
+    public static final String XPATH_SELECT_DAY = XPATH_SELECT + "/select[1]";
+    public static final String XPATH_SELECT_MONTH = XPATH_SELECT + "/select[2]";
+    public static final String XPATH_SELECT_YEAR = XPATH_SELECT + "/select[3]";
+
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.facebook.com");
+
+        while (!driver.findElement(By.xpath(XPATH_WAITFOR)).isDisplayed()) ;
+        //02.11.1999
+        WebElement selectComboDay = driver.findElement(By.xpath(XPATH_SELECT_DAY));
+        Select selectDay = new Select(selectComboDay);
+        selectDay.selectByIndex(2);
+
+        WebElement selectComboMonth = driver.findElement(By.xpath(XPATH_SELECT_MONTH));
+        Select selectMonth = new Select(selectComboMonth);
+        selectMonth.selectByIndex(11);
+
+        WebElement selectComboYear = driver.findElement(By.xpath(XPATH_SELECT_YEAR));
+        Select selectYear = new Select(selectComboYear);
+        selectYear.selectByIndex(20);
+    }
+}
